0631ff91b1d044e151bad678e15f180f58512bd1
Murat Lafci <arkantarkan.33@gmail.com>
2020-03-16 00:33:09 -0400

    our fifth commit

--- src/git_project01/GitDay2.java
+++ src/git_project01/GitDay2.java
@@ -5,7 +5,10 @@ public class GitDay2 {
 	public static void main(String[] args) {
 		
      System.out.println("Hello World");
-	
+	 System.out.println("Hello Git Users!");
+     
+     
+     
 	}
 
 }
